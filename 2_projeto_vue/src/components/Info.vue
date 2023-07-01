5fb710005a89c3fa5f2d30d87afd3d181c5d40a2
EduardoccsDev <eduardoc.info@gmail.com>
2023-07-01 13:19:32 -0300

    Directives

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="estaTrabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>JavaScript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p v-show="mostrarEmail">Mande uma mensagem para {{ email }}</p>
     </div>
 </template>
 
 <script>
     export default{
-        name:'Info'
+        name:'Info',
+        data(){
+            return{
+                estaTrabalhando: false,
+                mostrarEmail: true,
+                email: 'eduardoc.info@gmail.com'
+            }
+        }
     }
 </script>
