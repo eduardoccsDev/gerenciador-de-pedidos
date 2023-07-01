32838ec94266dbde46ba05676a844ea3d8472f64
EduardoccsDev <eduardoc.info@gmail.com>
2023-07-01 11:46:33 -0300

    Components Data

---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ name }}, ocupo a função de {{ occupation }}.</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'PrimeiroComponente',
+        data(){
+            return{
+                name: "Eduardo",
+                occupation: "Desenvolvedor"
+            }
+        }
+    }
+</script>
