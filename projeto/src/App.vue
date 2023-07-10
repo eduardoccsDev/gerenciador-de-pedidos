6f28f5e4ce8fae49d2a92bd75e70322e2dd09d00
EduardoccsDev <eduardoc.info@gmail.com>
2023-07-10 20:50:41 -0300

    Sidebar + sass added

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <Navbar :logo="logoSrc" :alt="appName"/>
-      <router-view/>
-  <Footer/>
+  <Sidebar/>
+  <main>
+    <router-view/>
+    <Footer/>
+  </main>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
-  components: { 
+  components: {
     Navbar,
-    Footer 
-  },
+    Footer,
+    Sidebar
+},
   data(){
     return{
       logoSrc: "/img/logo.png",
@@ -23,7 +27,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+:root{
+  --primary : #FFB711;
+  --primary-alt: #cc9411;
+  --gray: #737373;
+  --dark: #222;
+  --dark-alt: #181818;
+  --light: #fff;
+  --sidebar-width: 300px;
+}
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+body{
+  background-color: var(--light);
+}
+button{
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+#app{
+  display: flex;
+  main{
+    flex: 1 1 0 ;
+    padding:2rem;
+
+    @media(max-width: 768px){
+      padding-left: 6rem;
+    }
+  }
+}
 .main{
   min-height: 250px;
   margin-block: 30px;
---
+++ projeto/src/components/Sidebar.vue
@@ -0,0 +1,214 @@
+<template>
+    <aside :class="`${is_expanded && 'is-expanded'}`">
+        <div class="logo">
+            <router-link to="/">
+                <img src="/img/logo.png" alt="logo">
+            </router-link>
+        </div>
+        <div class="menu-toggle-wrap">
+            <button class="menu-toggle" @click="ToggleMenu">
+                <span class="icons">
+                    <i class="fa-solid fa-angle-right"></i>
+                </span>
+            </button>
+        </div>
+        <h3>Menu</h3>
+        <div class="menu">
+            <router-link to="/" class="buttonLink">
+                <span class="icons">
+                    <i class="fa-solid fa-house"></i>
+                </span>
+                <span class="text">
+                    Home
+                </span>
+            </router-link>
+            <router-link to="/sobre" class="buttonLink">
+                <span class="icons">
+                    <i class="fa-solid fa-eye"></i>
+                </span>
+                <span class="text">
+                    Sobre
+                </span>
+            </router-link>
+            <router-link to="/pedidos" class="buttonLink">
+                <span class="icons">
+                    <i class="fa-solid fa-boxes-stacked"></i>
+                </span>
+                <span class="text">
+                    Pedidos
+                </span>
+            </router-link>
+            <router-link to="/administracao" class="buttonLink">
+                <span class="icons">
+                    <i class="fa-solid fa-gears"></i>
+                </span>
+                <span class="text">
+                    Administração
+                </span>
+            </router-link>
+            <router-link to="/contato" class="buttonLink">
+                <span class="icons">
+                    <i class="fa-solid fa-address-book"></i>
+                </span>
+                <span class="text">
+                    Contato
+                </span>
+            </router-link>
+        </div>
+        <div class="flex"></div>
+        <div class="menu">
+            <router-link to="/configurações" class="buttonLink">
+                <span class="icons">
+                    <i class="fa-solid fa-gear"></i>
+                </span>
+                <span class="text">
+                    Configurações
+                </span>
+            </router-link>
+        </div>
+    </aside>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
+
+const ToggleMenu = () => {
+    is_expanded.value = !is_expanded.value
+    localStorage.setItem("is_expanded", is_expanded.value)
+}
+</script>
+
+<style lang="scss" scoped>
+aside {
+    display: flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 1rem;
+
+    background-color: var(--dark);
+    color: var(--light);
+
+    transition: 0.2s ease-out;
+
+    .flex{
+        flex: 1 1 0;
+    }
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            width: 5rem;
+        }
+    }
+
+    .menu-toggle-wrap {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+
+        position: relative;
+        top: 0;
+        transition: 0.2s ease-out;
+
+        .menu-toggle {
+            transition: 0.2s ease-out;
+
+            .icons i {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .icons i {
+                    color: var(--primary);
+                    transform: translateX(0.5rem);
+                }
+            }
+        }
+    }
+
+    h3,
+    .buttonLink .text {
+        opacity: 0;
+        transition: 0.3s ease-out;
+    }
+
+    h3 {
+        color: var(--gray);
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .menu {
+        margin: 0 -1rem;
+
+        .buttonLink {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+
+            padding: 0.5rem 1rem;
+            transition: 0.2s ease-out;
+
+            .icons i {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            .text {
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover,
+            &.router-link-exact-active {
+                background-color: var(--dark-alt);
+
+                .icons i,
+                .text {
+                    color: var(--primary);
+                }
+            }
+
+            &.router-link-exact-active {
+                border-right: 3px solid var(--primary);
+            }
+        }
+    }
+
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-wrap {
+            top: -3rem;
+
+            .menu-toggle {
+                transform: rotate(-180deg);
+            }
+        }
+
+        h3,
+        .buttonLink .text {
+            opacity: 1;
+        }
+
+        .buttonLink {
+            .icons i {
+                margin-right: 1rem;
+            }
+        }
+    }
+
+    @media(max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}</style>
